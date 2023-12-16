1e3326283f460365fc712f0353117b23485cb43c
Vadim Shikhovtsov <vadimx30@gmail.com>
2023-12-16 12:11:47 +0300

    Done. M4: task1-4 partially

---
+++ data/MainApp_item.sql
@@ -0,0 +1,6 @@
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (1,'Кроссовки','abibas',10,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (2,'Coca-cola 1 литр','coca-cola',12,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (3,'Картофель фри','вкусно и',0,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (4,'Куртка кожаная','nike',2,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (5,'Кепка','текстиль',124,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (6,'Вода газированная','Черноголовка',10,'Вода простая, вкусная');
